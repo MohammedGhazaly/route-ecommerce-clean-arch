9be346956828d089c2bf640bb6f7deea362bab1b
MohammedGhazaly <warendragon67@gmail.com>
2023-10-30 17:57:45 +0200

    Added models for error response

---
+++ lib/data/model/response_model/register_error_response_model.dart
@@ -0,0 +1,26 @@
+import 'package:route_e_commerce/data/model/response_model/error_model.dart';
+
+class RegisterResponseErrorModel {
+  String? statusMsg;
+  String? message;
+  ErrorModel? error;
+
+  RegisterResponseErrorModel({this.statusMsg, this.message, this.error});
+
+  RegisterResponseErrorModel.fromJson(Map<String, dynamic> json) {
+    statusMsg = json['statusMsg'];
+    message = json['message'];
+    error = json['errors'] != null ? ErrorModel.fromJson(json['errors']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['statusMsg'] = this.statusMsg;
+    data['message'] = this.message;
+    if (this.error != null) {
+      data['errors'] = this.error!.toJson();
+    }
+
+    return data;
+  }
+}
