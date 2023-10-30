ffabddffa0ac3e1968063a85a0017463b092e1ec
MohammedGhazaly <warendragon67@gmail.com>
2023-10-30 21:54:48 +0200

    Added register method in Api manager class

---
+++ lib/data/api/api_constants.dart
@@ -0,0 +1,4 @@
+class ApiConstants {
+  static const String baseUrl = "ecommerce.routemisr.com";
+  static const String registerEndPoint = "api/v1/auth/signup";
+}
--- lib/data/api/api_manager.dart
+++ lib/data/api/api_manager.dart
@@ -1,3 +1,31 @@
-import "package:http/http.dart" as http;
+import "dart:convert";
 
-class ApiManager {}
+import "package:http/http.dart" as http;
+import "package:route_e_commerce/data/api/api_constants.dart";
+import "package:route_e_commerce/data/model/request_models/register_request_model.dart";
+import "package:route_e_commerce/data/model/response_model/register_response_model.dart";
+
+class ApiManager {
+  static Future<RegisterResponseModel> register({
+    required String fullName,
+    required String phoneNumber,
+    required String email,
+    required String password,
+    required String rePassword,
+  }) async {
+    Uri url = Uri.https(ApiConstants.baseUrl, ApiConstants.registerEndPoint);
+
+    RegisterRequestModel registerRequestBody = RegisterRequestModel(
+      name: fullName,
+      phone: phoneNumber,
+      email: email,
+      password: password,
+      rePassword: rePassword,
+    );
+    http.Response response =
+        await http.post(url, body: registerRequestBody.toJson());
+    var registerResponseModel =
+        RegisterResponseModel.fromJson(jsonDecode(response.body));
+    return registerResponseModel;
+  }
+}
