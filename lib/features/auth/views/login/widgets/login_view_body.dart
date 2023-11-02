30248ef59ba61ba4c4553655df2198f4b100ccd2
MohammedGhazaly <warendragon67@gmail.com>
2023-11-02 23:25:12 +0200

    Added login view

--- lib/features/auth/views/login/widgets/login_view_body.dart
+++ lib/features/auth/views/login/widgets/login_view_body.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/foundation.dart';
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:route_e_commerce/features/auth/views/register/register_view.dart';
+import 'package:route_e_commerce/features/auth/views/shared_widgets/custom_auth_button.dart';
+import 'package:route_e_commerce/features/auth/views/shared_widgets/custom_auth_field.dart';
 
 class LoginViewBody extends StatefulWidget {
   const LoginViewBody({super.key});
@@ -10,7 +13,19 @@ class LoginViewBody extends StatefulWidget {
 }
 
 class _LoginViewBodyState extends State<LoginViewBody> {
+  final emailAddressController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool isPasswordVisible = false;
+
   final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    emailAddressController.dispose();
+    passwordController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,11 +70,104 @@ class _LoginViewBodyState extends State<LoginViewBody> {
                 height: 40.h,
               ),
               Form(
-                  autovalidateMode: AutovalidateMode.always,
-                  key: formKey,
-                  child: Column(
-                    children: [],
-                  ))
+                autovalidateMode: AutovalidateMode.always,
+                key: formKey,
+                child: Column(
+                  children: [
+                    CustomAuthField(
+                      textEditingController: emailAddressController,
+                      keyboardType: TextInputType.emailAddress,
+                      fieldName: "E-mail address",
+                      hintText: "enter your email address",
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return "Email should not be empty.";
+                        }
+                        if (!EmailValidator.validate(value)) {
+                          return "Please enter a valid email";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 32.h,
+                    ),
+                    CustomAuthField(
+                      textEditingController: passwordController,
+                      keyboardType: TextInputType.text,
+                      fieldName: "Password",
+                      hintText: "enter your password",
+                      isObscure: isPasswordVisible ? false : true,
+                      suffixIcon: InkWell(
+                        onTap: () {
+                          isPasswordVisible = !isPasswordVisible;
+                          setState(() {});
+                        },
+                        child: Icon(
+                          isPasswordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return "Password should not be empty.";
+                        }
+                        if (value.length < 6) {
+                          return "Enter at least 6 chars";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 16.h,
+                    ),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: InkWell(
+                        onTap: () {},
+                        child: Text(
+                          "Forgot password",
+                          style: TextStyle(
+                            fontSize: 18.sp,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 60.h,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: CustomAuthButton(
+                        text: "Login",
+                        isLoading: false,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 32.h,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context).pushNamed(RegisterView.routeName);
+                      },
+                      child: Text(
+                        "Don't have an account? Create one",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.sp,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 100.h,
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
